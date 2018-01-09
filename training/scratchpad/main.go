507c3ba1da252fc1bad5d3ef0fa94619bbf61f68
JavinForrester <Javin.Forrester@target.com>
2018-01-08 20:19:23 -0600

    fleshed out some practice

--- training/scratchpad/main.go
+++ training/scratchpad/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func nextFibNum(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func iseven(number int) bool {
+	if number%2 == 0 {
+		return true
+	}
+
+	return false
+}
 
 func main() {
-	fmt.Println((true && false) || (false && true) || !(false && false))
+	fibSeries := []int{1, 2}
+	fibSum := fibSeries[1]
+	fmt.Println(fibSeries)
+	fmt.Println(fibSum)
+
+	/* 	for i := 0; i >= 0; i++ {
+	   		if fibSeries[i]+fibSeries[i+1] >= 4000000 {
+	   			break
+	   		} else if iseven(nextFibNum(fibSeries[i], fibSeries[i+1])) {
+	   			fibSeries = append(fibSeries, nextFibNum(fibSeries[i], fibSeries[i+1]))
+	   			fibSum += fibSeries[len(fibSeries)-1]
+	   		}
+	   	}
+
+	   	fmt.Printf("%v\n", fibSum)
+	   	fmt.Println(fibSeries[len(fibSeries)-1])
+	*/
 }
